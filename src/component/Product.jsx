2574cf46663603411a9093be2fd58ddcc6ad9e9b
sharad-a <sharad.sttl@gmail.com>
2024-06-27 15:42:50 +0530

    prod fetching error resolved

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -51,7 +51,7 @@ const Products = ({ setCart, cart }) => {
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                 {products.map(product => (
                     <ProductCard
-                        key={product.id}
+                        key={product._id}
                         product={product}
                         addToCart={addToCart}
                     />
